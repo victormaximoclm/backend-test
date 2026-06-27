b1d20c5a43b4611f0b3be83902f4361e1d848515
Victor Maximo <victormaximoclima@gmail.com>
2026-06-27 14:59:29 -0300

    refactor: padroniza nomenclatura de Id para ID

--- internal/repository/part_repository.go
+++ internal/repository/part_repository.go
@@ -5,7 +5,7 @@ import "backend-test/internal/domain"
 //interface obrigatória que repositórios devem ter
 type PartRepository interface {
 	Create(part domain.Part) (domain.Part, error)
-	GetById(id string) (domain.Part, error)
+	GetByID(id string) (domain.Part, error)
 	List() ([]domain.Part, error)
 	ListByCategory(category string) ([]domain.Part, error)
 	Update(part domain.Part) (domain.Part, error)
